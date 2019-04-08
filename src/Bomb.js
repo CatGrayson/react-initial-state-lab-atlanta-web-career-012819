fd880beb898cf29dc12a29e9feb99a0a44c9c528
Chanda Grayson <chandagrayson@gmail.com>
2019-04-08 08:17:42 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,25 @@
 // your Bomb code here!
+import React from 'react'
+
+// In the src/Bomb.js file, create a Bomb React component.
+class Bomb extends React.Component {
+    constructor(props) {
+        super()
+
+        // Its initial state should have a property called secondsLeft.
+        this.state = {
+            // The initial value of secondsLeft is set by passing in an initialCount prop to the Bomb component. Don't forget to pass the argument props into the constructor (i.e., constructor(props)).
+            secondsLeft: props.initialCount
+        }
+    }
+    // It should render the text '<SECONDS_LEFT> seconds left before I go boom!', where <SECONDS_LEFT> is the value of secondsLeft.
+    render() {
+        // If secondsLeft equals 0, it should render 'Boom!' instead.
+        const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`
+
+        return (
+            <div>{message}</div>
+        )
+    }
+}
+export default Bomb
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,21 @@
 // your ImageSlider code here!
+import React from 'react'
+
+// In the src/ImageSlider.js file, create an ImageSlider React component.
+class ImageSlider extends React.Component {
+    constructor() {
+        super()
+
+        // Its initial state should have a property called currentSlideIndex that starts at 0.
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+    // It should only render out the text 'I am on slide <CURRENT_SLIDE>', where <CURRENT_SLIDE> is the value of this.state.currentSlideIndex.
+    render() {
+        return (
+            <h1>I am on slide {this.state.currentSlideIndex}</h1>
+        )
+    }
+}
+export default ImageSlider
